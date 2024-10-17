9aa2a4c906c52414b336dfa82b4e89ebc1c86bff
dhanasamudrala30 <saidhanalakshmi30112003@gmail.com>
2024-10-17 12:32:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InsuranceCards from './components/InsuranceCards';
+
+const App = () => {
+  return (
+    <div>
+      <InsuranceCards />
+    </div>
+  );
+};
+
+export default App;
